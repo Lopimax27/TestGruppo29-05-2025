46484156ab927c17e348009f842905e417562893
Emanuele Zignale <zignale92@gmail.com>
2025-05-30 16:56:38 +0200

    concrete class
    fantasy
    avventura
    horror

--- EsercizioDiGruppo/Emanuele.cs
+++ EsercizioDiGruppo/Emanuele.cs
@@ -1,19 +1,64 @@
 
-public abstract class Libro {
+public abstract class Libro
+{
     // Campi
     protected string titolo;
     protected int annoUscita;
     protected string autore;
 
     // Costruttore
-    public Libro(string titolo, string autore, int annoUscita) {
+    public Libro(string titolo, string autore, int annoUscita)
+    {
         this.titolo = titolo;
         this.annoUscita = annoUscita;
         this.autore = autore;
     }
 
     // Metodo astratto - da implementare nelle sottoclassi
-    public abstract String descrizioneLibro();
+    public abstract string descrizioneLibro();
+
+}
+public class LibroFantasy : Libro
+{
+    string genre = "fantasy";
+    public LibroFantasy(string titolo, string autore, int annoUscita, string genre) : base (titolo,autore,annoUscita)
+    {
+        this.genre = genre;
+    }
+
+    public override string descrizioneLibro()
+    {
+        return $"Libro {genre} ";
+    }
+
+}
+
+public class LibroAvventura : Libro
+{
+    string genre = "avventura";
+    public LibroAvventura(string titolo, string autore, int annoUscita, string genre) : base(titolo, autore, annoUscita)
+    {
+        this.genre = genre;
+    }
+
+    public override string descrizioneLibro()
+    {
+        return $"Libro {genre} ";
+    }
+
+}
+public class LibroHorror : Libro
+{
+    string genre = "horror";
+    public LibroHorror(string titolo, string autore, int annoUscita, string genre) : base (titolo,autore,annoUscita)
+    {
+        this.genre = genre;
+    }
+
+    public override string descrizioneLibro()
+    {
+        return $"Libro {genre} ";
+    }
 
 }
 public static class CreazioneLibri
